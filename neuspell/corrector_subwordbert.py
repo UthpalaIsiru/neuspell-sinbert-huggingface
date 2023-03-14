706eb46da839953b2fc16788720d1123c3a9cc95
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-14 15:05:46 +0530

    traning size reduced

--- neuspell/corrector_subwordbert.py
+++ neuspell/corrector_subwordbert.py
@@ -95,8 +95,8 @@ class BertChecker(Corrector):
         # training and validation
         #############################################
         model, vocab = self.model, self.vocab
-        # TRAIN_BATCH_SIZE, VALID_BATCH_SIZE = 8, 16
-        TRAIN_BATCH_SIZE, VALID_BATCH_SIZE = 16, 32
+        TRAIN_BATCH_SIZE, VALID_BATCH_SIZE = 8, 16
+        # TRAIN_BATCH_SIZE, VALID_BATCH_SIZE = 16, 32
         GRADIENT_ACC = 4
         DEVICE = self.device
         START_EPOCH, N_EPOCHS = 0, n_epochs

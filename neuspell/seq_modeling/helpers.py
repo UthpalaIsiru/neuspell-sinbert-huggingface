92a37fc211f28ff4e7a59ad9d19f0b43e3d6a040
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-28 18:33:12 +0530

    encode plus impl changed

--- neuspell/seq_modeling/helpers.py
+++ neuspell/seq_modeling/helpers.py
@@ -714,7 +714,7 @@ def bert_tokenize(batch_sentences):
 
     # max_seq_len = max([len(tokens) for tokens in batch_tokens])
     # batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens,max_length=max_seq_len,pad_to_max_length=True) for tokens in batch_tokens]
-    batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens.values.tolist()) for tokens in batch_tokens]
+    batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens) for tokens in batch_tokens]
 
     batch_attention_masks = pad_sequence(
         [torch.tensor(encoded_dict["attention_mask"]) for encoded_dict in batch_encoded_dicts], batch_first=True,
@@ -816,7 +816,7 @@ def bert_tokenize_for_valid_examples(batch_orginal_sentences, batch_noisy_senten
         max_seq_len = max([len(tokens) for tokens in batch_tokens])
         # batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens,add_special_tokens =True, max_length = 514,truncation=True) for tokens in batch_tokens]
         # batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens,max_length=514, add_special_tokens=True,  padding="max_length",truncation = True, is_split_into_words=True, return_attention_mask = True) for tokens in batch_tokens]
-        batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens) for tokens in batch_tokens]
+        batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens.values.tolist()) for tokens in batch_tokens]
 
         # batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(str(tokens),max_length=512,truncation = True) for tokens in batch_tokens]
 

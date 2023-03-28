9bc8bfd907677078c832017f118207c48b629e83
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-28 18:26:13 +0530

    encode plus impl changed

--- neuspell/seq_modeling/helpers.py
+++ neuspell/seq_modeling/helpers.py
@@ -714,7 +714,7 @@ def bert_tokenize(batch_sentences):
 
     # max_seq_len = max([len(tokens) for tokens in batch_tokens])
     # batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens,max_length=max_seq_len,pad_to_max_length=True) for tokens in batch_tokens]
-    batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens.tolist()) for tokens in batch_tokens]
+    batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens.values.tolist()) for tokens in batch_tokens]
 
     batch_attention_masks = pad_sequence(
         [torch.tensor(encoded_dict["attention_mask"]) for encoded_dict in batch_encoded_dicts], batch_first=True,

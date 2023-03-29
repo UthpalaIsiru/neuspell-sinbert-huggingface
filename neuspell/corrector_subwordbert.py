a93ba3e0fb313aca69c6bffd67ceda106cb4b7c8
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-29 10:59:12 +0530

    Logs added after labelize

--- neuspell/corrector_subwordbert.py
+++ neuspell/corrector_subwordbert.py
@@ -184,9 +184,12 @@ class BertChecker(Corrector):
                 batch_bert_inp = {k: v.to(DEVICE) for k, v in batch_bert_inp.items()}
                 # set batch data for others
                 batch_labels, batch_lengths = labelize(batch_labels, vocab)
+                print("===========after labelize in finetune")
+                print("batch_labels",batch_labels)
+                print("batch_lengths",batch_lengths)
                 # batch_lengths = batch_lengths.to(device)
                 batch_labels = batch_labels.to(DEVICE)
-
+                print("batch_labels.to(DEVICE) batch_labels",batch_labels)
                 # forward
                 model.train()
                 loss = model(batch_bert_inp, batch_bert_splits, targets=batch_labels)

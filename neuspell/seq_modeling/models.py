4453a9ca6cacf2e5a3e037b7e0634a4ddccb2a21
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-30 23:47:12 +0530

    print bert model

--- neuspell/seq_modeling/models.py
+++ neuspell/seq_modeling/models.py
@@ -903,7 +903,7 @@ class SubwordBert(nn.Module):
         # Uncomment to freeze BERT layers
         # for param in self.bert_model.parameters():
         #     param.requires_grad = False
-
+        print("self.bert_model",self.bert_model)
         # output module
         assert output_dim>0
         # self.dropout = nn.Dropout(p=0.4)

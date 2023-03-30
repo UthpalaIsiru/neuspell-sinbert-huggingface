db13f461dc141ae2833fe68c4ccd8ba7d2c73b60
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-30 11:35:43 +0530

    forward impl changed

--- neuspell/seq_modeling/models.py
+++ neuspell/seq_modeling/models.py
@@ -915,23 +915,33 @@ class SubwordBert(nn.Module):
         print("out", out)
         return out
 
+    
     def forward(self,
                 batch_bert_dict: "{'input_ids':tensor, 'attention_mask':tensor}",
-                # batch_bert_dict: "{'input_ids':tensor, 'attention_mask':tensor, 'token_type_ids':tensor}",
                 batch_splits: "list[list[int]]",
                 aux_word_embs: "tensor" = None,
                 targets: "tensor" = None,
-                topk=1): 
-        print("================debugging forward===============")
-        print("batch_bert_dict",batch_bert_dict)
+                topk = 1):
+        
         # cnn
         batch_size = len(batch_splits)
-        print("batch_size",batch_size)
+
         # bert
         # BS X max_nsubwords x self.bertmodule_outdim
-        bert_encodings = self.bert_model(batch_bert_dict["input_ids"],batch_bert_dict["attention_mask"], return_dict=False)
-        # bert_encodings = self.bert_model(**batch_bert_dict, return_dict=False)[0]
+        bert_encodings, cls_encoding = self.bert_model(
+            input_ids=batch_bert_dict["input_ids"],
+            attention_mask=batch_bert_dict["attention_mask"],
+            # token_type_ids=batch_bert_dict["token_type_ids"],
+        )
+        bert_encodings = self.bert_dropout(bert_encodings)
         print("bert_encodings",bert_encodings)
+        # BS X max_nwords x self.bertmodule_outdim
+        bert_merged_encodings = pad_sequence(
+            [self.get_merged_encodings(bert_seq_encodings, seq_splits, mode='avg') \
+                for bert_seq_encodings, seq_splits in zip(bert_encodings,batch_splits)],
+            batch_first=True,
+            padding_value=0
+        )
         # print("bert_encodings size",bert_encodings.ze.size())
         bert_encodings = self.bert_dropout(bert_encodings)
         # BS X max_nwords x self.bertmodule_outdim

f7dedd8f3f5246426a5dbee499bd8a5c15b2c7b3
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-03-31 09:31:40 +0530

    reverted: forward func def changed

--- neuspell/seq_modeling/models.py
+++ neuspell/seq_modeling/models.py
@@ -998,8 +998,8 @@ class SubwordBert(nn.Module):
 
     def forward(self,
                 # batch_bert_dict: "{'input_ids':tensor, 'attention_mask':tensor}",
-                input_ids: "tensor" = None,
-                attention_mask: "tensor" = None,
+                input_ids: "tensor",
+                attention_mask: "tensor",
                 batch_splits: "list[list[int]]",
                 aux_word_embs: "tensor" = None,
                 targets: "tensor" = None,

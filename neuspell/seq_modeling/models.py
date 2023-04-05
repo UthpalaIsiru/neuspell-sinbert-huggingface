3598bbd8901491a2763d84072fd641846234cc8e
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-04-05 14:15:27 +0530

    Logs added for forward

--- neuspell/seq_modeling/models.py
+++ neuspell/seq_modeling/models.py
@@ -1014,10 +1014,12 @@ class SubwordBert(nn.Module):
         # BS X max_nsubwords x self.bertmodule_outdim
         print("==============================================")
         print("================self.bert_model===============",self.bert_model(**batch_bert_dict, return_dict=False)[0])
-        bert_encodings, cls_encoding = self.bert_model(
-            input_ids=batch_bert_dict["input_ids"],
+        bert_encodings = self.bert_model(
+            batch_bert_dict["input_ids"],
             attention_mask=batch_bert_dict["attention_mask"],
-        )        # bert_encodings = self.bert_model(**batch_bert_dict, return_dict=False)[0]
+            return_dict=False
+        )[0]       
+         # bert_encodings = self.bert_model(**batch_bert_dict, return_dict=False)[0]
         print("bert_encodings",bert_encodings)
 
         bert_encodings = self.bert_dropout(bert_encodings)

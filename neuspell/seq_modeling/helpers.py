e68b949786ee485aa7d650845b26512c2fb1d474
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-04-20 15:01:51 +0530

    bert tokenizer changes done

--- neuspell/seq_modeling/helpers.py
+++ neuspell/seq_modeling/helpers.py
@@ -9,7 +9,7 @@ import torch
 import transformers
 from torch.nn.utils.rnn import pad_sequence
 from tqdm import tqdm
-# from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoModelForSequenceClassification
 
 BERT_MAX_SEQ_LEN = 512 #bert_max_seq_len
 BERT_TOKENIZER = None
@@ -772,15 +772,15 @@ def bert_tokenize_for_valid_examples(batch_orginal_sentences, batch_noisy_senten
     global BERT_TOKENIZER
     if BERT_TOKENIZER is None:  # gets initialized during the first call to this method
         if bert_pretrained_name_or_path:
-            BERT_TOKENIZER = transformers.XLMRobertaTokenizer.from_pretrained("NLPC-UOM/SinBERT-small")
+            # BERT_TOKENIZER = transformers.XLMRobertaTokenizer.from_pretrained("NLPC-UOM/SinBERT-small")
             # BERT_TOKENIZER = AutoTokenizer.from_pretrained("NLPC-UOM/SinBERT-small", add_prefix_space=True)
-            # BERT_TOKENIZER = AutoTokenizer.from_pretrained("NLPC-UOM/SinBERT-small", add_prefix_space=True,use_fast=False)
+            BERT_TOKENIZER = AutoTokenizer.from_pretrained("NLPC-UOM/SinBERT-small", add_prefix_space=True,use_fast=False)
             # BERT_TOKENIZER.do_basic_tokenize = True
             # BERT_TOKENIZER.tokenize_chinese_chars = False
         else:
-            BERT_TOKENIZER = transformers.XLMRobertaTokenizer.from_pretrained("NLPC-UOM/SinBERT-small")
+            # BERT_TOKENIZER = transformers.XLMRobertaTokenizer.from_pretrained("NLPC-UOM/SinBERT-small")
             # BERT_TOKENIZER = AutoTokenizer.from_pretrained("NLPC-UOM/SinBERT-small", add_prefix_space=True)
-            # BERT_TOKENIZER = AutoTokenizer.from_pretrained("NLPC-UOM/SinBERT-small", add_prefix_space=True,use_fast=False)
+            BERT_TOKENIZER = AutoTokenizer.from_pretrained("NLPC-UOM/SinBERT-small", add_prefix_space=True,use_fast=False)
             # BERT_TOKENIZER.do_basic_tokenize = True
             # BERT_TOKENIZER.tokenize_chinese_chars = False
 

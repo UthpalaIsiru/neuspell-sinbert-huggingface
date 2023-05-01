2ec1b1884ce71fa8443ff084901cfd86fb1bb225
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-05-01 18:23:22 +0530

    model output dim changed

--- neuspell/seq_modeling/models.py
+++ neuspell/seq_modeling/models.py
@@ -911,7 +911,8 @@ class SubwordBert(nn.Module):
         assert output_dim>0
         # self.dropout = nn.Dropout(p=0.4)
         # print("output_dim",output_dim)
-        self.dense = nn.Linear(self.bertmodule_outdim,output_dim)
+        self.dense = nn.Linear(30000,output_dim)
+        # self.dense = nn.Linear(self.bertmodule_outdim,output_dim)
         # print("self.dense",self.dense)
 
         # loss

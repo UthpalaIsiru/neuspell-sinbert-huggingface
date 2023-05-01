b9a4f54658607a2e941bf7a027b4f92616901f17
UthpalaPitawela <uthpala.isiru@gmail.com>
2023-05-01 18:01:29 +0530

    model output dim changed

--- neuspell/seq_modeling/models.py
+++ neuspell/seq_modeling/models.py
@@ -899,9 +899,9 @@ class SubwordBert(nn.Module):
 
         self.bert_dropout = torch.nn.Dropout(0.2)
         self.bert_model = AutoModelForMaskedLM.from_pretrained("NLPC-UOM/SinBERT-small")
-        self.bertmodule_outdim = self.bert_model.config.hidden_size
+        # self.bertmodule_outdim = self.bert_model.config.hidden_size
         # self.bert_model.resize_token_embeddings(768) #newly added to reduce the lm head token embedding size
-        # self.bertmodule_outdim = self.bert_model.config.vocab_size
+        self.bertmodule_outdim = self.bert_model.config.vocab_size
         # print("self.bert_model.config",self.bert_model.config)
         # Uncomment to freeze BERT layers
         # for param in self.bert_model.parameters():
